d9688257164f5c0c65fc6df541d836dd760cfbfc
ThomasKFlynn <d20125038@mytudublin.ie>
2021-04-24 14:55:43 +0100

    Pause menu and functionality added

--- bountyHunter_finalProject_thomasFlynnD20125038/Assets/Scripts/PauseMenu.cs
+++ bountyHunter_finalProject_thomasFlynnD20125038/Assets/Scripts/PauseMenu.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PauseMenu : MonoBehaviour
 {
@@ -42,11 +43,13 @@ public class PauseMenu : MonoBehaviour
 
     public void LoadMenu()
     {
+        Time.timeScale = 1f;
         Debug.Log("Loading....");
     }
 
     public void QuitGame()
     {
         Debug.Log("Quitting....");
+        Application.Quit();
     }
 }
